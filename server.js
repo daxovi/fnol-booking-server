9ffdfb7374226effe7f3e874b838cb54c030eb07
Dalibor Janeček <dalibor.janecek@gmail.com>
2023-12-28 19:52:27 +0100

    emails

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -25,6 +26,36 @@ app.use("/", cors(), getTickets);
 // POST
 app.use("/", saveMaterial);
 app.use("/", saveTicket);
+
+// Konfigurace Nodemaileru
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST, // Váš SMTP server
+    port: process.env.SMTP_PORT, // Port SMTP serveru
+    secure: false, // true pro 465, false pro ostatní porty
+    auth: {
+        user: process.env.SMTP_USER, // Uživatelské jméno pro SMTP server
+        pass: process.env.SMTP_PASSWORD, // Heslo pro SMTP server
+    },
+});
+
+// Endpoint pro odeslání e-mailu
+app.post("/send-email", (req, res) => {
+    const { to, subject, text } = req.body; // Získání dat z požadavku
+
+    const mailOptions = {
+        from: process.env.SMTP_USER, // E-mailová adresa odesílatele
+        to: to, // Příjemce
+        subject: subject, // Předmět
+        text: text, // Text e-mailu
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send("E-mail byl úspěšně odeslán: " + info.response);
+    });
+});
 
 app.get("/", (req, res) => {
     res.send("jsi na hlavní stránce");
